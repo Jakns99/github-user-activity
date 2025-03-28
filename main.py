eb3426208bb6d1f42616f9b716b2a079170a76f8
Jakns99 <j.schermerhorn99@gmail.com>
2025-03-27 23:48:36 -0700

    Added first if statement for CreateEvent

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def get_recent_activity(username):
     response = requests.get(url) # Get request for the API url
     if response.status_code == 200:
         github_data = response.json()
+        events = github_data
+        for event in events:
+            if event['type'] == "CreateEvent": # Lists all CreateEvent actions taken recently by user
+                rich_print(f"{username} created event: {event['payload']['ref_type']} {event['payload']['ref']}")
     else:
         print(f"\033[1;31mPlease enter a valid username.\0330m")
 
