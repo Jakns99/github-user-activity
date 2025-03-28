3d252f083f8b02c03071aa95c041a8a82fba7600
Jakns99 <j.schermerhorn99@gmail.com>
2025-03-27 23:28:27 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+from rich import print as rich_print
+
+# TODO 1. Get username for GitHub as a Command Line Argument
+# TODO 2. Do a basic check if the username provided is valid
+# TODO 3. Fetch the user data from GitHub API
+# TODO 4. API error? Show an error message and exit
+# TODO 5. API Success? Process the JSON data
+# TODO 6. No activity? Show an information message and exit
+# TODO 7. Activity found? Show the activity in a readable format, and exit.
+
+github_api_url = "https://api.github.com/users" # GitHub API URL reference
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+
+
