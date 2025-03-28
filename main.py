b0f070281a82db35dcd9bec66faf2e7570c8f039
Jakns99 <j.schermerhorn99@gmail.com>
2025-03-27 23:32:50 -0700

    Changed TODOs and created new function for fetching the recent activity of users.

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import requests
 import sys
 from rich import print as rich_print
 
-# TODO 1. Get username for GitHub as a Command Line Argument
-# TODO 2. Do a basic check if the username provided is valid
-# TODO 3. Fetch the user data from GitHub API
-# TODO 4. API error? Show an error message and exit
-# TODO 5. API Success? Process the JSON data
-# TODO 6. No activity? Show an information message and exit
-# TODO 7. Activity found? Show the activity in a readable format, and exit.
-
+# TODO 1. Have user provide GitHub username as an argument when running the CLI
+# TODO 2. Fetch recent activity of GH user using API
+# TODO 3. Display the fetched activity in the terminal, using JSON
+# TODO 4. Handle errors gracefully
+# TODO 5. Filter by Event type
+# TODO 6. Cache fetched data
 github_api_url = "https://api.github.com/users" # GitHub API URL reference
 
-
+# Fetch recent event activity from username
 #----------------------------------------------------------------------------------------------------------------------#
+def get_recent_activity(username):
+    """Fetches the recent activity of the provided username"""
 
 
 
